dfb3e20d60ebffa566cde45114f812a3d6b4d574
Rachel Voirin <rvoirin3@gatech.edu>
2020-10-17 19:30:19 -0400

    Cart Page now works on mobile and little bug fixes

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -10,7 +10,7 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 function CartItem(props) {
 
-    const { name, quantity, price } = props;
+    const { name, quantity, price, canDelete } = props;
     const element = <FontAwesomeIcon icon={faTrash} />
 
     return (
@@ -18,18 +18,20 @@ function CartItem(props) {
             <Card>
                 <Card.Body>
                     <Row style={{'width':'100%'}}>
-                        <Col>
+                        <Col sm={1} xs={2}>
                             <Card.Title className="mb-2 text-muted"> { quantity } </Card.Title>
                         </Col>
-                        <Col>
+                        <Col sm = {8} xs={6}>
                             <Card.Title>{name}</Card.Title>
                         </Col>
-                        <Col>
+                        <Col sm = {2} xs={3}>
                             <Card.Title className="mb-2 text-muted"> { price }</Card.Title>
                         </Col>
-                        <Col style={{"paddingRight":"0px"}}>
-                            <Button variant={'outline-danger'} className={"float-right"} style={{'marginTop':'7px'}}>{element}</Button>
-                            <Button variant={'outline-success'} className={"float-right"} style={{'marginTop':'7px'}}>+</Button> 
+                        <Col sm= {1} style={{"paddingRight":"0px"}} xs={1}>
+                            { canDelete &&
+                            <Button variant={'outline-danger'} className={"float-right"} style={{'marginTop':'7px'}}>{element}</Button> 
+                            }
+                            {/* <Button variant={'outline-success'} className={"float-right"} style={{'marginTop':'7px'}}>+</Button>  */}
                         </Col>
                     </Row>
                 </Card.Body>
