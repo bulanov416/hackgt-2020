086068a4ea4a5e0a6e00637a00b5eab6982f7b92
Alex Bulanov <alexander@bulanov.com>
2020-10-17 19:59:45 -0400

    Retrieves menu items from firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react' 
 
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
@@ -7,8 +7,47 @@ import Col from 'react-bootstrap/Col';
 
 import Menu from './pages/Menu.js';
 import Cart from './pages/Cart.js';
+import MenuItemObject from './models/MenuItemObject';
+
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUrlthvN7-mzjShK52AfthkX6yepCuwgk",
+    authDomain: "hackgt2020-5847e.firebaseapp.com",
+    databaseURL: "https://hackgt2020-5847e.firebaseio.com",
+    projectId: "hackgt2020-5847e",
+    storageBucket: "hackgt2020-5847e.appspot.com",
+    messagingSenderId: "838591600721",
+    appId: "1:838591600721:web:829023369df328b7a18070"
+};
 
 function App() {
+    useEffect(() => {
+        let menuItemsArray = [];
+        firebase.initializeApp(firebaseConfig);
+        const database = firebase.firestore();
+        database.collection('restaurants').get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                let restaurantId = doc.id;
+                console.log(restaurantId);
+                database.collection('restaurants').doc(restaurantId).collection('menuItems').get().then(function(querySnapshot2) {
+                    querySnapshot2.forEach(function(doc2) {
+                        let category = doc2.get('Category');
+                        let description = doc2.get('Description');
+                        let name = doc2.get('Name');
+                        let currentMenuItem = new MenuItemObject(category, name, description); 
+                        menuItemsArray.push(currentMenuItem);
+                        for (let i = 0; i < menuItemsArray.length; i++) {
+                            console.log("Cat: ", menuItemsArray[i].category, " || ", "Desc: ", menuItemsArray[i].description, " || ", "Name: ", menuItemsArray[i].name);
+                        }
+                    })
+                });
+            })
+        }).catch(function(error) {
+            console.log("Error getting documents: ", error);
+        });
+    }, []);
+
   return (
     <div>
         <Navbar bg="light" expand="lg">
---
+++ client/src/models/MenuItemObject.js
@@ -0,0 +1,7 @@
+export default class MenuItemObject {
+    constructor(category, name, description) {
+        this.category = category;
+        this.name = name;
+        this.description = description;
+    }
+}
