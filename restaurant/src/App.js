a18a230564395abf79086a0517665aa01efd2ebf
Alex Bulanov <alexander@bulanov.com>
2020-10-17 10:37:10 -0400

    started restaurant app

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Table from './components/Table.js';  
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">Jerry's Berrys</Navbar.Brand>
+            <Button variant="outline-info">Select Restaurant</Button>
+            <Button variant="outline-info">View Active Orders</Button>
+            <Button variant="outline-info">Edit Menu</Button>
+        </Navbar>
+        <Container>
+            <Table name={"Table 1"} description={"Hamburger"}/>
+            <Table name={"Table 2"} description={"Salad"}/>
+            <Table name={"Table 3"} description={"Smoothie"}/>
+        </Container>
     </div>
   );
 }
