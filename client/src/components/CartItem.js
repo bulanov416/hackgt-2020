9a0c933eabc5d6c69d9f29d31f506b30b8cde04d
Rachel Voirin <rvoirin3@gatech.edu>
2020-10-17 19:09:52 -0400

    Added delete(trash) button and some columns for the car rows

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -5,9 +5,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from "react-bootstrap/Card";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
 function CartItem(props) {
 
     const { name, quantity, price } = props;
+    const element = <FontAwesomeIcon icon={faTrash} />
 
     return (
         <>
@@ -15,13 +19,17 @@ function CartItem(props) {
                 <Card.Body>
                     <Row style={{'width':'100%'}}>
                         <Col>
+                            <Card.Title className="mb-2 text-muted"> { quantity } </Card.Title>
+                        </Col>
+                        <Col>
                             <Card.Title>{name}</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted"> { quantity } </Card.Subtitle>
-                            <Card.Subtitle className="mb-2 text-muted"> { price }</Card.Subtitle>
+                        </Col>
+                        <Col>
+                            <Card.Title className="mb-2 text-muted"> { price }</Card.Title>
                         </Col>
                         <Col style={{"paddingRight":"0px"}}>
-                            <Button variant={'outline-success'} className={"float-right"} style={{'marginTop':'7px'}}>+</Button>
-                            <Button variant={'outline-success'} className={"float-right"} style={{'marginTop':'7px'}}>-</Button>
+                            <Button variant={'outline-danger'} className={"float-right"} style={{'marginTop':'7px'}}>{element}</Button>
+                            <Button variant={'outline-success'} className={"float-right"} style={{'marginTop':'7px'}}>+</Button> 
                         </Col>
                     </Row>
                 </Card.Body>
