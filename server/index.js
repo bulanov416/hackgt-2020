5695ee4efbc131f6a78b48d1981cee66b1f88358
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-10-18 04:53:42 -0400

    Moved into build directory

--- server/index.js
+++ server/index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'client-build')));
-app.use(express.static(path.join(__dirname, 'restaurant-build')));
+app.use(express.static(path.join(__dirname, 'build/client-build')));
+app.use(express.static(path.join(__dirname, 'build/restaurant-build')));
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
 app.get('/client/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client-build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build/client-build', 'index.html'));
 });
 
 app.get('/restaurant/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'restaurant-build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build/restaurant-build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
