8f0687c93b3d41c76e81478ad3970c0b0aa0a589
Murali, Shweta <smurali62@gatech.edu>
2020-10-18 01:39:44 -0400

    added dining progress page

--- client/src/pages/Dining.js
+++ client/src/pages/Dining.js
@@ -1,1 +1,29 @@
-//page for once you've ordered can request a waiter or refills etc
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+
+function Dining() {
+    return(
+        <Container style={{'padding':'100px'}}>
+            <h1 class="text-center">
+                Dining in Progress...
+            </h1>
+            <br></br><br></br>
+            <Button variant="primary" size="lg" block>
+                Request Refill
+            </Button>
+            <Button variant="secondary" size="lg" block>
+                Request Waiter
+            </Button>
+            <Button variant="primary" size="lg" block>
+                Order More
+            </Button>
+            <Button variant="secondary" size="lg" block>
+                Leave Restaurant
+            </Button>
+        </Container>
+    );
+}
+
+export default Dining;
+
