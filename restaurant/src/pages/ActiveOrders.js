aaf436fd164151df72a219a43f7525668d4dad3d
Alex Bulanov <alexander@bulanov.com>
2020-10-18 03:00:05 -0400

    order status update button functionality added

--- restaurant/src/pages/ActiveOrders.js
+++ restaurant/src/pages/ActiveOrders.js
@@ -4,10 +4,11 @@ import '../static/css/ActiveOrders.css';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import loading from "../static/assets/loading_gif.gif";
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 import OrderItem from "../components/OrderItem";
 
@@ -20,7 +21,7 @@ function ActiveOrders(props) {
 
     const [ currentTable, setCurrentTable ] = useState(1);
     const [ orders, setOrders ] = useState([]);
-    const [ status, setStatus ] = useState("orderPending");
+    const [ status, setStatus ] = useState("Order Pending");
 
     const tableHandler = (tableNum) => {
         setCurrentTable(tableNum);
@@ -99,12 +100,42 @@ function ActiveOrders(props) {
                         <h1 style={{"width":"100%"}}>Table {currentTable}</h1>
                         <Container style={{'padding':'0px'}}>
                             {
-                                (orders != null) ? orderItems :
+                                (orders != {}) ? orderItems :
                                 <div style={{'width': '100%', 'textAlign': 'center'}}>
                                     <img src={loading} style={{'width': '20px'}} alt='loading...'/>
                                 </div>
                             }
                         </Container>
+                    </Col>
+                </Row>
+                <Row className={"justify-content-center fix-bottom"} style={{"width":"32%"}}>
+                    <Col style={{"paddingRight":"0px"}}>
+                        <Button size={'lg'} variant={"success"} style={{"width":"100%"}} onClick={() => {
+                            let nextStatus = "";
+                            if (status == "Order Pending") {
+                                nextStatus = "Order Received";
+                            } else if (status == "Order Received") {
+                                nextStatus = "Food Ready";
+                            } else if (status == "Food Ready") {
+                                nextStatus = "Meal Finished";
+                            } else if (status == "Meal Finished") {
+                                nextStatus = "Table Sanitized - Press to Reset";
+                            } else if (status == "Table Sanitized - Press to Reset") {
+                                nextStatus = "Order Pending";
+                                database.collection('restaurants').doc('rFxSt5fTT3Kp4NphAomx').collection('tables').doc('table'+currentTable).set({
+                                    orders: {},
+                                    status: "Order Pending"
+                                })
+                                .then(function() {
+                                    document.location = "/";
+                                })
+                            }
+                            setStatus(nextStatus);
+                            database.collection('restaurants').doc('rFxSt5fTT3Kp4NphAomx').collection('tables').doc('table'+currentTable).set({
+                                orders: orders,
+                                status: nextStatus
+                            })
+                        }}>{status}</Button>
                     </Col>
                 </Row>
             </div>
